30135a6eec0c282a06fd075420c2d38a50510317
Ewa Jackiewicz <jackiewicz.ewa@wp.pl>
2019-01-14 13:20:14 +0100

    [2] props.children

--- src/index.js
+++ src/index.js
@@ -8,20 +8,25 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>Are you sure?</ApprovalCard>
       <ApprovalCard>
         <CommentDetail
           avatar={faker.image.avatar()}
           author="Sam" timeAgo="Today at 1:23PM"
           content="Great post."/>
       </ApprovalCard>
+      <ApprovalCard>
         <CommentDetail
           avatar={faker.image.avatar()}
           author="Alex" timeAgo="Today at 9:05AM"
           content="I disagree..."/>
+      </ApprovalCard>
+      <ApprovalCard>
         <CommentDetail
           avatar={faker.image.avatar()}
           author="Jane" timeAgo="Yesterday at 6:18PM"
           content="Please visit my blog!"/>
+      </ApprovalCard>
     </div>
   );
 };
